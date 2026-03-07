af128e4b1f3c329a3bb97472b371e0ec11c29103
doniliks <lavrovdanylo@gmail.com>
2026-03-08 00:06:44 +0100

    Changed sorting of LeaderboardController.kt. Adjusted tests.

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -22,9 +22,9 @@ class LeaderboardControllerTests {
 
     @Test
     fun test_getLeaderboard_correctScoreSorting() {
-        val first = GameResult(1, "first", 20, 20.0)
+        val first = GameResult(1, "first", 10, 20.0)
         val second = GameResult(2, "second", 15, 10.0)
-        val third = GameResult(3, "third", 10, 15.0)
+        val third = GameResult(3, "third", 20, 15.0)
 
         whenever(mockedService.getGameResults()).thenReturn(listOf(second, first, third))
 
@@ -32,13 +32,13 @@ class LeaderboardControllerTests {
 
         verify(mockedService).getGameResults()
         assertEquals(3, res.size)
-        assertEquals(first, res[0])
+        assertEquals(third, res[0])
         assertEquals(second, res[1])
-        assertEquals(third, res[2])
+        assertEquals(first, res[2])
     }
 
     @Test
-    fun test_getLeaderboard_sameScore_CorrectIdSorting() {
+    fun test_getLeaderboard_sameScore_SortedByLowerTime() {
         val first = GameResult(1, "first", 20, 20.0)
         val second = GameResult(2, "second", 20, 10.0)
         val third = GameResult(3, "third", 20, 15.0)
@@ -49,9 +49,9 @@ class LeaderboardControllerTests {
 
         verify(mockedService).getGameResults()
         assertEquals(3, res.size)
-        assertEquals(first, res[0])
-        assertEquals(second, res[1])
-        assertEquals(third, res[2])
+        assertEquals(second, res[0])
+        assertEquals(third, res[1])
+        assertEquals(first, res[2])
     }
 
 }
